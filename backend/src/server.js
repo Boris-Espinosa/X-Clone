c973ffe08480c58cb8ec188906577664c1449cdf
Boris Espinosa <borisem.dev@gmail.com>
2025-09-17 20:56:28 -0300

    db-models created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+connectDB()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on http://localhost:${ENV.PORT}`);
+})
